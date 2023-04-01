9faf521625521f34f7f99d04a825dc0ab9a59130
Thomas Luechtefeld <tom@insilica.co>
2023-04-01 17:26:10 -0400

    minor compound changes

--- pubchem/part_1_compound_sdf.R
+++ pubchem/part_1_compound_sdf.R
@@ -1,7 +1,7 @@
 pacman::p_load(tidyverse, biobricks, ggplot2)
 
-pc <- bbload("pubchem")
-pc <- pc$compound_sdf 
+# LOAD PUBCHEM COMPOUND SDF TABLE
+pc <- bbload("pubchem")$compound_sdf
 
 # HOW MANY ROWS?
 nrow(pc) / 1e9
@@ -14,6 +14,17 @@ props <- pc |> select(property) |> distinct()
 props <- props |> pull(property,as_vector=FALSE)
 
 # GET SOME MOLECULAR WEIGHTS?
-mw <- pc |> 
-  filter(property == "PUBCHEM_MOLECULAR_WEIGHT") |> 
-  select(id,value) |> collect() |> tibble()
+mw <- pc |> filter(property == "PUBCHEM_MOLECULAR_WEIGHT") |> collect()
+
+# too big for memory
+process_pcfile <- function(pcfile){
+  df <- arrow::read_parquet(pcfile, as_data_frame=FALSE) |> 
+    filter(property == "PUBCHEM_MOLECULAR_WEIGHT") |> 
+    collect()
+
+  df |> mutate(value = as.integer(value)) |> count(value,sort=TRUE)
+}
+
+pacman::p_load(future)
+future::plan(future::multisession(workers=6))
+mwcounts <- pc$files |> furrr::future_map(process_pcfile)
