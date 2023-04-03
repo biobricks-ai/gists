2a30f9f5ac81c1be8b5c856dc0421dbdbb047a8f
Thomas Luechtefeld <tom@insilica.co>
2023-04-02 22:11:19 -0400

    finished https://youtu.be/gFiLjr0G4sM

--- pubchem/part_1_compound_sdf.R
+++ pubchem/part_1_compound_sdf.R
@@ -1,30 +1,32 @@
-pacman::p_load(tidyverse, biobricks, ggplot2)
+pacman::p_load(biobricks, tidyverse, ggplot2, tictoc, arrow)
 
-# LOAD PUBCHEM COMPOUND SDF TABLE
-pc <- bbload("pubchem")$compound_sdf
+# LOAD PUBCHEM
+pc <- biobricks::bbload("pubchem")
+pc <- pc$compound_sdf
 
 # HOW MANY ROWS?
 nrow(pc) / 1e9
 
-# A GLIMPSE?
-pc |> head(100) |> collect() |> tibble()
+# A GLIMPSE
+pctop <- pc |> head(1000) |> collect() |> tibble()
+unique(pctop$property)
 
-# WHAT ARE THE PROPERTIES?
-props <- pc |> select(property) |> distinct()
-props <- props |> pull(property,as_vector=FALSE)
+# GET MOLECULAR WEIGHTS!
+tic()
+mw <- pc |> filter(property == "PUBCHEM_MOLECULAR_WEIGHT")
+mw <- mw |> mutate(value = as.integer(round(as.numeric(value))))
+mw <- mw |> group_by(value) |> summarize(cnt=n())
+mw <- mw |> collect()
+toc()
 
-# GET SOME MOLECULAR WEIGHTS?
-mw <- pc |> filter(property == "PUBCHEM_MOLECULAR_WEIGHT") |> collect()
+# HOW MANY MOLECULAR WEIGHTS?
+total = sum(mw$cnt)
 
-# too big for memory
-process_pcfile <- function(pcfile){
-  df <- arrow::read_parquet(pcfile, as_data_frame=FALSE) |> 
-    filter(property == "PUBCHEM_MOLECULAR_WEIGHT") |> 
-    collect()
+# TOP 99% MOLECULAR WEIGHTS
+mw <- mw |> arrange(value) |> mutate(percentile = cumsum(cnt) / total)
+mw99 <- mw |> filter(percentile > 0.98) |> pull(value) |> first()
 
-  df |> mutate(value = as.integer(value)) |> count(value,sort=TRUE)
-}
-
-pacman::p_load(future)
-future::plan(future::multisession(workers=6))
-mwcounts <- pc$files |> furrr::future_map(process_pcfile)
+# PLOT
+df <- mw |> filter(value < mw99)
+g <- ggplot(df, aes(x=value, y=cnt)) + geom_col() 
+ggsave("pubchem_mol_weights.png")
